9a62358ddff7882bd12f8f0aa3d55b8c95460d8f
CristopherMRodriguez <59444688+brogrammerJesus@users.noreply.github.com>
2021-01-04 18:56:53 -0500

    use app.use to serve css file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-app.get("/", function(req, res){
-  res.send("Hello Express");
-})
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
 
 
 
