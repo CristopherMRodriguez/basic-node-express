6bf9276f0004515bfff31cc0220ae37227a1a2cc
CristopherMRodriguez <59444688+brogrammerJesus@users.noreply.github.com>
2021-01-05 17:36:13 -0500

    added middleware and middleware chaining

--- myApp.js
+++ myApp.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 require("dotenv").config();
 
+app.use((req, res, next)=>{
+  var string = req.method + " " + req.path + " - " + req.ip;
+  next();
+})
+
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({time: req.time});
+})
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
@@ -13,8 +25,6 @@ app.get("/json", function(req, res) {
   }
     else return res.json({"message": message});
   })
-
-  
 
 app.use(express.static(__dirname + "/public"));
 
