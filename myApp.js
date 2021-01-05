9efcb07b3eca0e56b7a13b53e543029ccd4b9ec8
CristopherMRodriguez <59444688+brogrammerJesus@users.noreply.github.com>
2021-01-05 15:53:12 -0500

    add .env file and variable

--- myApp.js
+++ myApp.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
+require("dotenv").config();
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", function(req, res) {
-  res.json({"message": "Hello json"});
-})
+  let message = "Hello json"
+  if (process.env.MESSAGE_STYLE == "uppercase"){
+    return res.json({"message": message.toUpperCase()}) 
+  }
+    else return res.json({"message": message});
+  })
+
+  
 
 app.use(express.static(__dirname + "/public"));
 
