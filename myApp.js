e1886798168b51355001eb1cd8c33cc6c0a1bd9f
CristopherMRodriguez <59444688+brogrammerJesus@users.noreply.github.com>
2021-01-06 13:04:19 -0500

    destructured query request and handled it with json response

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 require("dotenv").config();
+var bodyParser = require("body-parser")
 
 app.use((req, res, next)=>{
   var string = req.method + " " + req.path + " - " + req.ip;
@@ -12,6 +13,18 @@ app.get("/now", (req, res, next) => {
   next();
 }, function(req, res) {
   res.json({time: req.time});
+})
+
+app.get("/name", (req, res) => {
+  var { first: firstName, last: lastName } = req.query;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+})
+
+app.get("/:word/echo", (req, res) =>{
+  var {word} = req.params;
+  res.json({echo: word});
 })
 
 app.get("/", function(req, res) {
